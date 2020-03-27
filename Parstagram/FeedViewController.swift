3d78234c3be49cb422e7abdd94d956144d4a949a
Grace <54523668+graceleung204@users.noreply.github.com>
2020-03-27 00:55:44 -0700

    updated

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -40,8 +40,20 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableview
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        
         let post = posts[indexPath.row]
+        let user = post["author"] as! PFUser
+        cell.usernameLabel.text = user.username
+        cell.captionLabel.text = (post["caption"] as! String)
+        
+        let imageFile = post["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        
+        cell.photoView.af_setImage(withURL: url)
+        
+        return cell
     }
 
     /*
