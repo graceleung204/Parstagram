5209057b713e48d58d9a4a2fc8049390ce5133ae
Grace <54523668+graceleung204@users.noreply.github.com>
2020-03-26 23:56:04 -0700

    updated

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -7,15 +7,42 @@
 //
 
 import UIKit
+import Parse
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-class FeedViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    var posts = [PFObject]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
 
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let query = PFQuery(className: "Posts")
+        query.includeKey("author")
+        query.limit = 20
+        
+        query.findObjectsInBackground{(posts, error) in
+            if posts != nil {
+                self.posts = posts!
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableview
+        let post = posts[indexPath.row]
+    }
 
     /*
     // MARK: - Navigation
